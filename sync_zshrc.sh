ac60aef465b26eb6e4f6d30e6f76731d78791192
Tai Duong <imbaggaarm@gmail.com>
2021-07-25 02:03:26 +0700

    Use DIR_PATH instead of DIR_NAME

--- sync_zshrc.sh
+++ sync_zshrc.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-DIR_NAME=`dirname "$0"`
+DIR_PATH=`dirname "$0"`
 
-/opt/homebrew/bin/fswatch -o $HOME/.zshrc $HOME/.zsh/ | xargs -n1 zsh $DIR_NAME/commit_changes.sh
+/opt/homebrew/bin/fswatch -o $HOME/.zshrc $HOME/.zsh/ | xargs -n1 zsh $DIR_PATH/commit_changes.sh
